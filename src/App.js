d82d3696b237062d46c7964609cc11def6d18114
Eddah Karambu <gikundaeddah@gmail.com>
2022-02-02 14:47:15 +0300

    added additional buttons on app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>FITNESS APP</h1>
+      <h2>Targer Muscle</h2>
+      <h3>Select Target muscle</h3>
+      <div className="targets">
+        <button>Neck</button>
+        <button>Shoulder</button>
+        <button>Chest</button>
+        <button>Abs</button>
+        <button>Legs</button>
+
+      </div>
 
     </div>
   );
