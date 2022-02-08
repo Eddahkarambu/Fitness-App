6fc613c16008aa4c44356202fc33f58d82457189
Eddah Karambu <gikundaeddah@gmail.com>
2022-02-08 16:28:38 +0300

    added images file , added a sign in button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,13 +2,18 @@ import './home.css';
 function Home() {
     return (
       <div className="home">
-          <div Heading>
+          <div className="Heading">
           <h1>Eddah's Fast Fitness</h1>
           </div>
-          <div Heading2>
+
+          <div classNmae="Heading2">
           <h2>SHORTER WORKOUTS</h2>
           <h2>BETTER RESULTS</h2>
           <h3>SHORT MANAGEABLE WORKOUTS FOR PEOPLE WITH NO TIME</h3>
+          </div>
+
+          <div className="button">
+              <button>Sign in</button>
           </div>
       </div>
     );
