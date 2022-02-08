fa899f152119874a12e2eae1ea55d0c2066bd781
Eddah Karambu <gikundaeddah@gmail.com>
2022-02-08 16:06:37 +0300

    created components folder, home.js  and home.css files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-
+import React  from "react";
+import './index.css';
+import Home from './Components/Home.js';
 
 function App() {
   return (
     <div className="App">
-      <div className="All">
-      <h1>FITNESS APP</h1>
-      <h2>Targer Muscle</h2>
-      <h3>Select Target muscle</h3>
-      <div className="targets">
-        <button>Neck</button>
-        <button>Shoulder</button>
-        <button>Chest</button>
-        <button>Abs</button>
-        <button>Legs</button>
-
-      </div>
-      </div>
-
+      <Home />
+      
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import './home.css';
+function Home() {
+    return (
+      <div className="home">
+          <div Heading>
+          <h1>Eddah's Fast Fitness</h1>
+          </div>
+          <div Heading2>
+          <h2>SHORTER WORKOUTS</h2>
+          <h2>BETTER RESULTS</h2>
+          <h3>SHORT MANAGEABLE WORKOUTS FOR PEOPLE WITH NO TIME</h3>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Home;
