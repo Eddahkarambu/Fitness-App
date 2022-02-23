de28290c817abaaa91fbd8bed6705c58c2fff8d0
Eddah Karambu <gikundaeddah@gmail.com>
2022-02-23 13:06:30 +0300

    used event handlers so that the form can respond to data

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -5,28 +5,46 @@ function Signup() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    
+
+    const [submitted, setSubmitted] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleName = (e) => {
+        setName(e.target.value);
+        setSubmitted(false);
+      };
+
+      const handleEmail = (e) => {
+        setEmail(e.target.value);
+        setSubmitted(false);
+      };
+
+      const handlePassword = (e) => {
+        setPassword(e.target.value);
+        setSubmitted(false);
+      };
+  
     return (
       <div className="Sign">
           <div className="form">
             <form >
-                    <label className="label">Name</label>
-                    <input  className="input"
-                    value={name} type="text" />
+                    <label className="label">Name</label>           
+                    <input onChange={handleName} className="input"
+                    value={name} type="text"/>
 
                     <label className="label">Email</label>
-                    <input className="input"
-                    value={email} type="email" />
+                    <input onChange={handleEmail} className="input"
+                    value={email} type="email"/>
 
                     <label className="label">Password</label>
-                    <input className="input"
-                    value={password} type="password" />
+                    <input onChange={handlePassword} className="input"
+                    value={password} type="password"/>
 
                     <label className="label">Confirm password</label>
-                    <input className="input"
-                    value={password} type="password" />
+                    <input onChange={handlePassword} className="input"
+                    value={password} type="password"/>
 
-                    <button  className="btn" type="submit">
+                    <button onClick={handleSubmit} className="btn" type="submit">
                     Submit
                     </button>
             </form> 
