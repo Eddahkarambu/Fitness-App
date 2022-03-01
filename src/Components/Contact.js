79c2916132d662f310224a6f5d19464d91e87e66
Eddah Karambu <gikundaeddah@gmail.com>
2022-03-01 13:13:29 +0300

    sign in page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,8 +3,24 @@ import './Contact.css';
 
 function Contact() {
     return(
-        <div>
-            fghhghjh
+        <div className="Contacts">
+           <Navbar />
+           <div className="details">
+           {/* <form>
+                    <label className="label">Email</label>
+                    <input onChange={handleEmail} className="input"
+                    value={email} type="email" />
+
+                    <label className="label">Password</label>
+                    <input onChange={handlePassword} className="input"
+                    value={password} type="password" />
+
+                    <button onClick={handleSubmit} className="button" type="submit">
+                   Submit
+                    </button>
+            </form>  */}
+
+           </div>
         </div>
 
     )
